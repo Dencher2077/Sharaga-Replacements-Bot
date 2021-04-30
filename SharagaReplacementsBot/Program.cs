9281205fc93bad197b8d4d28990b9e4df8e935ec
Dencher2077 <dencher2077@gmail.com>
2021-04-30 10:33:13 +0300

    some refactoring

--- SharagaReplacementsBot/Program.cs
+++ SharagaReplacementsBot/Program.cs
@@ -23,32 +23,36 @@ namespace SharagaReplacementsBot
         private static async void OnMessage(object sender, MessageEventArgs e)
         {
             var msg = e.Message;
-
             Console.WriteLine($"Message: {msg.Text}");
             
             if(msg.Text == "/start")
                 await Client.SendTextMessageAsync(msg.Chat.Id, "Здарова ёпта", replyMarkup: GetButtons());
             
             if (msg.Text.ToLower() is "/reps" or "замены")
-            {
-                try
-                {
-                    await Client.SendTextMessageAsync(msg.Chat.Id, "Погодь...");
-                    await Client.SendTextMessageAsync(msg.Chat.Id, await SharagaReplacement.GetReplacementsString("https://cutt.ly/3vA4E5x"));
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                }
-            }
-
+                SendReplacements(msg.Chat.Id);
+            
             if (msg.Text.ToLower() is "/week_type" or "тип недели")
+                SendWeekType(msg.Chat.Id);
+        }
+
+        private static async void SendWeekType(long chatId)
+        {
+            int x = (new DateTime(2021, 3, 26) - new DateTime()).Days / 7;
+            string result = x % 2 == 0 ? "Числитель" : "Знаменатель";
+            await Client.SendTextMessageAsync(chatId, result);
+        }
+        
+        private static async void SendReplacements(long chatId)
+        {
+            try
             {
-                int x = (new DateTime(2021, 3, 26) - new DateTime()).Days / 7;
-                string result = x % 2 == 0 ? "Числитель" : "Знаменатель";
-                await Client.SendTextMessageAsync(msg.Chat.Id, result);
+                await Client.SendTextMessageAsync(chatId, "Погодь...");
+                await Client.SendTextMessageAsync(chatId, await SharagaReplacement.GetReplacementsString("https://cutt.ly/3vA4E5x"));
             }
-                
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
 
         private static ReplyKeyboardMarkup GetButtons()
